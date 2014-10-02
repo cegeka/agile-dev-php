114f97066523ad29d007a7c9e4bbc55ac9054547
Petre Pătraşc <petre@dreamlabs.ro>
2014-10-02 12:12:59 +0000

    Removed test solutions for Array exercise

--- src/Arrays/ArrayManipulatorTest.php
+++ src/Arrays/ArrayManipulatorTest.php
@@ -4,9 +4,6 @@ require_once 'ArrayManipulator.php';
 
 class ArrayManipulatorTest extends \PHPUnit_Framework_TestCase
 {
-    /** @var array */
-    protected $defaultValidData = [1, 2, 3];
-
     /** @var ArrayManipulator */
     protected $arrayManipulatorClass;
 
@@ -15,172 +12,38 @@ class ArrayManipulatorTest extends \PHPUnit_Framework_TestCase
         $this->arrayManipulatorClass = new ArrayManipulator();
     }
 
-    /**
-     * @param $data
-     * @param $expectedResponse
-     * @dataProvider elementsProvider
-     */
-    public function testLoad($data, $expectedResponse)
+    public function testLoad()
     {
-        $actualResponse = $this->arrayManipulatorClass->load($data);
-
-        $this->assertInternalType('array', $actualResponse);
-        $this->assertEquals($expectedResponse, $actualResponse, 'Invalid data has been loaded into the manipulator.');
+        $this->markTestIncomplete();
     }
 
-    public function elementsProvider()
+    public function testAddElement()
     {
-        return [
-            [[1, 2, 3], [1, 2, 3]],
-            [[-1, 2, 3], [2, 3]],
-            [[1.5, -2.2, 3], [3]],
-            [[-1, 2.2, 3.3], []],
-            [[], []],
-        ];
+        $this->markTestIncomplete();
     }
 
-    /**
-     * @param $element
-     * @param $expectedResponse
-     * @dataProvider singleElementProvider
-     */
-    public function testAddElement($element, $expectedResponse)
+    public function testSort()
     {
-        $this->arrayManipulatorClass->load($this->defaultValidData);
-        $actualResponse = $this->arrayManipulatorClass->addElement($element);
-
-        $this->assertInternalType('array', $actualResponse);
-        $this->assertEquals($expectedResponse, $actualResponse, 'Adding an element has not generated the expected results.');
+        $this->markTestIncomplete();
     }
 
-    public function singleElementProvider()
+    public function testSum()
     {
-        return [
-            [4, $this->defaultValidData + [3 => 4]],
-            [0, $this->defaultValidData + [3 => 0]],
-            [-2, $this->defaultValidData],
-            [-2.2, $this->defaultValidData],
-            [2.2, $this->defaultValidData],
-        ];
+        $this->markTestIncomplete();
     }
 
-    /**
-     * @param $inputData
-     * @param $expectedResponse
-     * @dataProvider sortDataProvider
-     */
-    public function testSort($inputData, $expectedResponse)
+    public function testElementsMultipleOf()
     {
-        $this->arrayManipulatorClass->load($inputData);
-        $actualResponse = $this->arrayManipulatorClass->sort();
-
-        $this->assertInternalType('array', $actualResponse);
-        $this->assertEquals($expectedResponse, $actualResponse, 'Sorting the array has not generated the expected results.');
+        $this->markTestIncomplete();
     }
 
-    public function sortDataProvider()
+    public function testRaiseToPower()
     {
-        return [
-            [[1, 2, 3], [1, 2, 3]],
-            [[3, 2, 1], [1, 2, 3]],
-            [[2, 3, 1], [1, 2, 3]],
-            [[-12, 3, 1], [1, 3]],
-        ];
+        $this->markTestIncomplete();
     }
 
-    /**
-     * @param $inputData
-     * @param $expectedResponse
-     * @dataProvider sumDataProvider
-     */
-    public function testSum($inputData, $expectedResponse)
+    public function testToString()
     {
-        $this->arrayManipulatorClass->load($inputData);
-        $actualResponse = $this->arrayManipulatorClass->sum();
-
-        $this->assertInternalType('int', $actualResponse);
-        $this->assertEquals($expectedResponse, $actualResponse, 'The array sum was not calculated correctly.');
-    }
-
-    public function sumDataProvider()
-    {
-        return [
-            [[1, 2, 3], 6],
-            [[2.2, 3], 3],
-            [[1, 2, 3, 4, 5], 15],
-        ];
-    }
-
-    /**
-     * @param $inputData
-     * @param $expectedResponse
-     * @param $multiple
-     * @dataProvider multipleElementsDataProvider
-     */
-    public function testElementsMultipleOf($inputData, $multiple, $expectedResponse)
-    {
-        $this->arrayManipulatorClass->load($inputData);
-        $actualResponse = $this->arrayManipulatorClass->elementsMultipleOf($multiple);
-
-        $this->assertInternalType('array', $actualResponse);
-        $this->assertEquals($expectedResponse, $actualResponse, 'The array sum was not calculated correctly.');
-    }
-
-    public function multipleElementsDataProvider()
-    {
-        return [
-            [[1, 2, 3], 2, [2]],
-            [[3, 6, 9], 3, [3, 6, 9]],
-            [[1, 2, 3], 1, [1, 2, 3]],
-        ];
-    }
-
-    /**
-     * @param $inputData
-     * @param $power
-     * @param $expectedResponse
-     * @dataProvider raiseToPowerDataProvider
-     */
-    public function testRaiseToPower($inputData, $power, $expectedResponse)
-    {
-        $this->arrayManipulatorClass->load($inputData);
-        $actualResponse = $this->arrayManipulatorClass->raiseToPower($power);
-
-        $this->assertEquals($expectedResponse, $actualResponse, 'Raising the array to a specific power was not computed correctly.');
-    }
-
-    public function raiseToPowerDataProvider()
-    {
-        return [
-            [[1, 2, 3], 2, [1, 4, 9]],
-            [[3, 6, 9], 2, [9, 36, 81]],
-            [[1, 2, 3], 3, [1, 8, 27]],
-            [[1, 2, 3], -3, null],
-            [[1, 2, 3], 0, null],
-        ];
-    }
-
-    /**
-     * @param $inputData
-     * @param $expectedResponse
-     * @dataProvider toStringDataProvider
-     */
-    public function testToString($inputData, $expectedResponse)
-    {
-        $this->arrayManipulatorClass->load($inputData);
-        $actualResponse = (string)$this->arrayManipulatorClass;
-
-        $this->assertInternalType('string', $actualResponse);
-        $this->assertEquals($expectedResponse, $actualResponse, 'Class toString did not match expected string.');
-    }
-
-    public function toStringDataProvider()
-    {
-        return [
-            [[1, 2, 3], 'Data: 1, 2, 3'],
-            [[-2, 2, 3], 'Data: 2, 3'],
-            [[-2, 2.2, 3], 'Data: 3'],
-            [[-2, 2.2, 3.3], 'No data is stored.'],
-        ];
+        $this->markTestIncomplete();
     }
 } 
